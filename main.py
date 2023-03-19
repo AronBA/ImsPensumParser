0ae20b8fd61d8b4b02ee97c0506fbadbd019776c
Aron <aronbaur@gmx.ch>
2023-03-19 06:11:54 +0100

    improve runtime by using a cache
    By caching the last page the runtime was reduced from 5minutes to 5seconds

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ from pdfminer.high_level import extract_pages
 from pdfminer.layout import LTTextBoxHorizontal
 
 start_time = banana.time()
-
+lastroom = None
+cache = None
 pages = list(extract_pages("data1.pdf", page_numbers=[0]))
 
 
@@ -18,11 +19,25 @@ def getCoords(element):
                     return l
 
 
-def getRoomInfo(day, time, room):
+def getPageCache(room):
+    global lastroom
+    global cache
+
+    if lastroom == room:
+        lastroom = room
+        return cache
+    else:
+        lastroom = room
+        cache = list(extract_pages("data1.pdf", page_numbers=[room]))
+        return cache
+
+
+def getRoomInfo(day, time, page):
     day = getCoords(f"{day}\n")[1]
     time = getCoords(f"{time}\n")[0]
     desc = []
-    for p in extract_pages("data1.pdf", page_numbers=[room]):
+
+    for p in page:
         for e in p:
             if isinstance(e, LTTextBoxHorizontal):
                 if day - 20 <= e.y0 <= day + 20 and time - 5 <= e.x0 <= time + 5:
@@ -40,33 +55,29 @@ def getTimes():
 
 
 def getRooms():
-    return ['Th1', 'Th2', 'Th3', 'Th4', 'Th5', '10', '11', '12', '105', '106', '107', '108', '110', '113', '114', '117',
-            '118', '120', '121', '123', '124', '204', '205', '206', '207', '211', '215', '220', '221', '222', '223',
-            '224', '226', '227', "311", "313", "318", "319", "320", "321", "322", "324", "325", '411', '412', '413',
-            '415', '418', '419', '421', '422', '423', '424', '425']
-
-
-def roomnametoint(room):
-    return getRooms().index(room)
+    return ["G1a", "G1b", "G1c", "G1d", "G1e", "G1f", "G2a", "G2b", "G2c", "G2d", "G2e", "G3a", "G3b", "G3c", "G3e",
+            "G4a", "G4b", "G4c", "G4d", "G4e", "W1a",
+            "W1b", "W1c", "W1d", "W2a", "W2b", "W2c", "W2d", "W3a", "W3b", "W3c", "I1a", "I1b", "I2a", "I2b", "I3a",
+            "I3b"]
 
 
 def buildJson(file_path):
     d = {"data": {}}
     counter = 0
     for r in getRooms():
-        if counter == 1:
-            print(f"Estimated time: {(banana.time() - start_time) * len(getRooms()) / 60} minutes")
+
         print(f"file parsed {counter}/{len(getRooms())} after {banana.time() - start_time} seconds")
         counter += 1
         d["data"][r] = {}
         for dy in getDays():
             d["data"][r][dy] = {}
             for t in getTimes():
-                d["data"][r][dy][t] = getRoomInfo(dy, t, roomnametoint(r))
+                d["data"][r][dy][t] = getRoomInfo(dy, t, getPageCache(getRooms().index(r)))
 
     with open(file_path, "w") as f:
         json.dump(d, f)
 
 
-buildJson("out.json")
+buildJson("zimmer.json")
+
 print(f"Building the Json took: {banana.time() - start_time} seconds")
